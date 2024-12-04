4d9683800b2298966d012daa803ea25f4043ac80
Strangekim <114288288+Strangekim@users.noreply.github.com>
2024-12-04 20:37:42 +0900

    Yhk trackingservice (#8)
    * 머지 테스트
    
    * 닉네임
    
    * 기본 셋팅 1
    
    * 기본 셋팅 추가
    
    * 기본 셋팅 중
    
    * 트래킹 서비스 작성 중
    
    ---------
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-10-222.ap-northeast-2.compute.internal>

--- src/router/tracking/service.js
+++ src/router/tracking/service.js
@@ -1,1 +1,46 @@
-const tmp = 0;
+const client = require("../../database/postgreSQL")
+const customError = require("../../middleware/customError")
+
+// ================================== 공통 함수 ============================
+
+// 구글 맵에서 사용되는 데이터 PostgreGIS 형태로 변환
+
+function convertMultiLine(line) {
+    const multiLine = line.map((section) => 
+        `(${section.map((point) => `${point.lng} ${point.lat}`).join(", ")})`
+    ).join(", ");
+
+    return `MULTILINESTRING(${multiLine})`
+}
+
+// DB에서 꺼내 온 값 구글 맵에 활용할 형태로 변환
+
+function convertFromMultiLine(multiLine) {
+    // 'MULTILINE' 제거하고 양쪽 괄호 삭제
+    let cleanedString = multiLine.replace("MULTILINESTRING", "").trim();
+    cleanedString = cleanedString.slice(1, -1); // 양쪽 괄호 제거
+
+    // 각 라인을 나누기
+    const lines = cleanedString.split("), (").map(line => line.replace("(", "").replace(")", ""));
+
+    // 각 라인을 좌표 배열로 변환
+    const parsedLines = lines.map((line) => {
+        return line.split(", ").map((point) => {
+            const [lng, lat] = point.split(" ").map(Number); // 경도와 위도를 숫자로 변환
+            return { lat, lng }; // 객체로 반환
+        });
+    });
+
+    return parsedLines;
+}
+
+// ===================================== 서비스 ===================================
+
+// 트래킹 이미지 생성
+const createTrackingImg = async (req,res,next) => {
+    const {line,searchPoint,center,zoom,sharing,color,thickness,background} = req.body
+
+
+
+    const multiLine = convertMultiLine(line)
+}
--- src/router/tracking/tracking.js
+++ src/router/tracking/tracking.js
@@ -1,1 +1,36 @@
-const tmp = 0;
+const router = require("express").Router()
+
+// 트래킹 이미지 생성
+router.post("/",
+
+)
+
+// 나의 트래킹 이미지 가져오기
+router.get("/",
+
+)
+
+// 다른 사용자의 전체 트래킹 이미지 가져오기
+router.get("/account/:idx",
+
+)
+
+// 나의 트래킹 이미지 삭제
+router.delete("/:idx",
+
+)
+
+// 트래킹 라인 가져오기
+router.get("/:idx",
+
+)
+
+// 트래킹 이미지 수정
+router.put("/:idx",
+
+)
+
+// 트래킹 이미지 공유 상태 변경
+router.put("/:idx",
+    
+)
