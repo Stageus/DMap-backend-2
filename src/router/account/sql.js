b59fe16d9c5321e4a20ff3a73174c0ee2f5c2b55
Ubuntu <ubuntu@ip-172-31-4-106.ap-northeast-2.compute.internal>
2025-01-07 10:48:59 +0000

    account API

--- src/router/account/sql.js
+++ src/router/account/sql.js
@@ -1,1 +1,54 @@
-const tmp = 0;
+// 체크 sql문 ---------------------------------
+const checkNicknameSql = "SELECT * FROM account.list WHERE nickname=$1;";
+
+// 회원가입 sql문 --------------------------------
+const postAccountGoogleSql =
+  "INSERT INTO account.list (google_user_id, nickname) VALUES ($1, $2);";
+const postAccountKakaoSql =
+  "INSERT INTO account.list (kakao_user_id, nickname) VALUES ($1, $2);";
+const postAccountNaverSql =
+  "INSERT INTO account.list (naver_user_id, nickname) VALUES ($1, $2);";
+
+// 리프레쉬토큰 관련 sql문 -------------------------------------------
+const getRefreshTokenSql =
+  "SELECT idx, expires_at FROM account.list WHERE refresh_token = $1;";
+const putRefreshTokenSql =
+  "UPDATE account.list SET refresh_token=$1, expires_at=$2 WHERE idx=$3;";
+
+// 회원 정보 가져오기 sql문 ------------------------------------------
+const getAccountSql =
+  "SELECT idx, nickname, img_url FROM account.list WHERE idx=$1;";
+const getUserIdxGoogleSql =
+  "SELECT idx FROM account.list WHERE google_user_id=$1;";
+const getUserIdxKakaoSql =
+  "SELECT idx FROM account.list WHERE kakao_user_id=$1;";
+const getUserIdxNaverSql =
+  "SELECT idx FROM account.list WHERE naver_user_id=$1;";
+
+// 회원 정보 변경 sql문 -------------------------------------------
+const putNicknameSql = "UPDATE account.list SET nickname=$1 WHERE idx=$2;";
+const putImageSql = "UPDATE account.list SET img_url=$1 WHERE idx=$2;";
+
+// 회원 탈퇴 sql문 -------------------------------------------------
+const deleteAccountSql = "DELETE FROM account.list WHERE idx=$1;";
+
+module.exports = {
+  checkNicknameSql,
+
+  postAccountGoogleSql,
+  postAccountKakaoSql,
+  postAccountNaverSql,
+
+  getAccountSql,
+  getUserIdxGoogleSql,
+  getUserIdxKakaoSql,
+  getUserIdxNaverSql,
+
+  getRefreshTokenSql,
+  putRefreshTokenSql,
+
+  putNicknameSql,
+  putImageSql,
+
+  deleteAccountSql,
+};
